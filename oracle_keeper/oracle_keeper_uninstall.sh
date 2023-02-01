cad9f883b4e10aa59e4273247cf3a04ecff11b21
neno <healneno@gmail.com>
2023-02-01 10:43:03 +0800

    update

--- oracle_keeper/oracle_keeper_uninstall.sh
+++ oracle_keeper/oracle_keeper_uninstall.sh
@@ -1,5 +1,5 @@
 systemctl stop oracle_keeper
 systemctl disable oracle_keeper
-rm /usr/local/oracle_keeper/oracle_keeper.py
+rm -rf /usr/local/oracle_keeper
 rm /usr/lib/systemd/system/oracle_keeper.service
 systemctl daemon-reload
