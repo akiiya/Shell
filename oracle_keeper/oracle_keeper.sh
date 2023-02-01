ebffa0c0d21cec184864ef2280a28dba877c5d5e
neno <healneno@gmail.com>
2023-02-01 10:36:16 +0800

    update

--- oracle_keeper/oracle_keeper.sh
+++ oracle_keeper/oracle_keeper.sh
@@ -3,7 +3,6 @@
 mkdir -p /usr/local/oracle_keeper
 wget  -N --no-check-certificate https://raw.githubusercontent.com/akiiya/Shell/master/oracle_keeper/oracle_keeper.py -O /usr/local/oracle_keeper/oracle_keeper.py
 wget  -N --no-check-certificate https://raw.githubusercontent.com/akiiya/Shell/master/oracle_keeper/oracle_keeper.service -O /usr/lib/systemd/system/oracle_keeper.service
-chmod +x /etc/systemd/system/oracle_keeper.service
 systemctl daemon-reload
 systemctl enable oracle_keeper
 
